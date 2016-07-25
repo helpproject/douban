f77df29844cb9c17a27f31a0ecda8ed61f7011a3
project <1045255717@qq.com>
2016-07-25 22:42:05 +0800

    我是来给文通擦屁股的

--- douban/resources/views/layout/admin.blade.php
+++ douban/resources/views/layout/admin.blade.php
@@ -5,39 +5,6 @@
 <!--[if IE 8]>    <html class="lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--><html lang="en"><!--<![endif]-->
 <head>
-<<<<<<< HEAD
-<meta charset="utf-8">
-@section('css')
-<!-- Viewport Metatag -->
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-
-<!-- Plugin Stylesheets first to ease overrides -->
-<link rel="stylesheet" type="text/css" href="/admins/plugins/colorpicker/colorpicker.css" media="screen">
-<link rel="stylesheet" type="text/css" href="/admins/custom-plugins/wizard/wizard.css" media="screen">
-
-<!-- Required Stylesheets -->
-<link rel="stylesheet" type="text/css" href="/admins/bootstrap/css/bootstrap.min.css" media="screen">
-<link rel="stylesheet" type="text/css" href="/admins/css/fonts/ptsans/stylesheet.css" media="screen">
-<link rel="stylesheet" type="text/css" href="/admins/css/fonts/icomoon/style.css" media="screen">
-
-<link rel="stylesheet" type="text/css" href="/admins/css/mws-style.css" media="screen">
-<link rel="stylesheet" type="text/css" href="/admins/css/icons/icol16.css" media="screen">
-<link rel="stylesheet" type="text/css" href="/admins/css/icons/icol32.css" media="screen">
-
-<!-- Demo Stylesheet -->
-<link rel="stylesheet" type="text/css" href="/admins/css/demo.css" media="screen">
-
-<!-- jQuery-UI Stylesheet -->
-<link rel="stylesheet" type="text/css" href="/admins/jui/css/jquery.ui.all.css" media="screen">
-<link rel="stylesheet" type="text/css" href="/admins/jui/jquery-ui.custom.css" media="screen">
-
-<!-- Theme Stylesheet -->
-<link rel="stylesheet" type="text/css" href="/admins/css/mws-theme.css" media="screen">
-<link rel="stylesheet" type="text/css" href="/admins/css/themer.css" media="screen">
-<link rel="stylesheet" href="/admins/css/my.css">
-@show
-<title>@yield('title')</title>
-=======
     <meta charset="utf-8">
 
     <!-- Viewport Metatag -->
@@ -71,42 +38,11 @@
     <link rel="stylesheet" href="/admins/css/my.css">
 
     <title>@yield('title')</title>
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
 
 </head>
 
 <body>
 
-<<<<<<< HEAD
-	<!-- Header -->
-	<div id="mws-header" class="clearfix">
-    
-    	<!-- Logo Container -->
-    	<div id="mws-logo-container">
-        
-        	<!-- Logo Wrapper, images put within this wrapper will always be vertically centered -->
-        	<div id="mws-logo-wrap">
-                <span style="font-size:30px;color:white">144</span>
-			</div>
-        </div>
-        
-        <!-- User Tools (notifications, logout, profile, change password) -->
-        <div id="mws-user-tools" class="clearfix">
-        
-        	<!-- Notifications -->
-        	<div id="mws-user-notif" class="mws-dropdown-menu">
-            	<a href="#" data-toggle="dropdown" class="mws-dropdown-trigger"><i class="icon-exclamation-sign"></i></a>
-                
-                <!-- Unread notification count -->
-                <span class="mws-dropdown-notif">35</span>
-                
-                <!-- Notifications dropdown -->
-                <div class="mws-dropdown-box">
-                	<div class="mws-dropdown-content">
-                        <ul class="mws-notifications">
-                        	<li class="read">
-                            	<a href="#">
-=======
 <!-- Header -->
 <div id="mws-header" class="clearfix">
 
@@ -135,90 +71,42 @@
                     <ul class="mws-notifications">
                         <li class="read">
                             <a href="#">
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
                                     <span class="message">
                                         Lorem ipsum dolor sit amet consectetur adipiscing elit, et al commore
                                     </span>
                                     <span class="time">
                                         January 21, 2012
                                     </span>
-<<<<<<< HEAD
-                                </a>
-                            </li>
-                        	<li class="read">
-                            	<a href="#">
-=======
                             </a>
                         </li>
                         <li class="read">
                             <a href="#">
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
                                     <span class="message">
                                         Lorem ipsum dolor sit amet
                                     </span>
                                     <span class="time">
                                         January 21, 2012
                                     </span>
-<<<<<<< HEAD
-                                </a>
-                            </li>
-                        	<li class="unread">
-                            	<a href="#">
-=======
                             </a>
                         </li>
                         <li class="unread">
                             <a href="#">
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
                                     <span class="message">
                                         Lorem ipsum dolor sit amet
                                     </span>
                                     <span class="time">
                                         January 21, 2012
                                     </span>
-<<<<<<< HEAD
-                                </a>
-                            </li>
-                        	<li class="unread">
-                            	<a href="#">
-=======
                             </a>
                         </li>
                         <li class="unread">
                             <a href="#">
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
                                     <span class="message">
                                         Lorem ipsum dolor sit amet
                                     </span>
                                     <span class="time">
                                         January 21, 2012
                                     </span>
-<<<<<<< HEAD
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="mws-dropdown-viewall">
-	                        <a href="#">View All Notifications</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <!-- Messages -->
-            <div id="mws-user-message" class="mws-dropdown-menu">
-            	<a href="#" data-toggle="dropdown" class="mws-dropdown-trigger"><i class="icon-envelope"></i></a>
-                
-                <!-- Unred messages count -->
-                <span class="mws-dropdown-notif">35</span>
-                
-                <!-- Messages dropdown -->
-                <div class="mws-dropdown-box">
-                	<div class="mws-dropdown-content">
-                        <ul class="mws-messages">
-                        	<li class="read">
-                            	<a href="#">
-                                    <span class="sender">John Doe</span>
-=======
                             </a>
                         </li>
                     </ul>
@@ -243,165 +131,45 @@
                         <li class="read">
                             <a href="#">
                                 <span class="sender">John Doe</span>
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
                                     <span class="message">
                                         Lorem ipsum dolor sit amet consectetur adipiscing elit, et al commore
                                     </span>
                                     <span class="time">
                                         January 21, 2012
                                     </span>
-<<<<<<< HEAD
-                                </a>
-                            </li>
-                        	<li class="read">
-                            	<a href="#">
-                                    <span class="sender">John Doe</span>
-=======
                             </a>
                         </li>
                         <li class="read">
                             <a href="#">
                                 <span class="sender">John Doe</span>
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
                                     <span class="message">
                                         Lorem ipsum dolor sit amet
                                     </span>
                                     <span class="time">
                                         January 21, 2012
                                     </span>
-<<<<<<< HEAD
-                                </a>
-                            </li>
-                        	<li class="unread">
-                            	<a href="#">
-                                    <span class="sender">John Doe</span>
-=======
                             </a>
                         </li>
                         <li class="unread">
                             <a href="#">
                                 <span class="sender">John Doe</span>
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
                                     <span class="message">
                                         Lorem ipsum dolor sit amet
                                     </span>
                                     <span class="time">
                                         January 21, 2012
                                     </span>
-<<<<<<< HEAD
-                                </a>
-                            </li>
-                        	<li class="unread">
-                            	<a href="#">
-                                    <span class="sender">John Doe</span>
-=======
                             </a>
                         </li>
                         <li class="unread">
                             <a href="#">
                                 <span class="sender">John Doe</span>
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
                                     <span class="message">
                                         Lorem ipsum dolor sit amet
                                     </span>
                                     <span class="time">
                                         January 21, 2012
                                     </span>
-<<<<<<< HEAD
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="mws-dropdown-viewall">
-	                        <a href="#">View All Messages</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <!-- User Information and functions section -->
-            <div id="mws-user-info" class="mws-inset">
-            
-            	<!-- User Photo -->
-            	<div id="mws-user-photo">
-                	<img src="/admins/example/profile.jpg" alt="User Photo">
-                </div>
-                
-                <!-- Username and Functions -->
-                <div id="mws-user-functions">
-                    <div id="mws-username">
-                        Hello, John Doe
-                    </div>
-                    <ul>
-                    	<li><a href="#">Profile</a></li>
-                        <li><a href="#">Change Password</a></li>
-                        <li><a href="index.html">Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    <!-- Start Main Wrapper -->
-    <div id="mws-wrapper">
-    
-    	<!-- Necessary markup, do not remove -->
-		<div id="mws-sidebar-stitch"></div>
-		<div id="mws-sidebar-bg"></div>
-        
-        <!-- Sidebar Wrapper -->
-        <div id="mws-sidebar">
-        
-            <!-- Hidden Nav Collapse Button -->
-            <div id="mws-nav-collapse">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            
-        	<!-- Searchbox -->
-        	<div id="mws-searchbox" class="mws-inset">
-            	<form action="typography.html">
-                	<input type="text" class="mws-search-input" placeholder="Search...">
-                    <button type="submit" class="mws-search-submit"><i class="icon-search"></i></button>
-                </form>
-            </div>
-            
-            <!-- Main Navigation -->
-            <div id="mws-navigation">
-                <ul>
-                    <li>
-                        <a href="#"><i class="icon-list"></i> 分类管理</a>
-                        <ul class="closed">
-                            <li><a href="/admin/cate/add">分类添加</a></li>
-                            <li><a href="/admin/cate/index">分类列表</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>         
-        </div>
-        
-        <!-- Main Container Start -->
-        <div id="mws-container" class="clearfix">
-        
-        	<!-- Inner Container Start -->
-            <div class="container">
-                @section('content')
-
-                @show
-            </div>
-            <!-- Inner Container End -->
-                       
-            <!-- Footer -->
-            <div id="mws-footer">
-            	Copyright Your Website 2012. All Rights Reserved.
-            </div>
-            
-        </div>
-        <!-- Main Container End -->
-        
-    </div>
-    @section('js')
-=======
                             </a>
                         </li>
                     </ul>
@@ -533,17 +301,12 @@
 
 </div>
 @section('js')
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
     <!-- JavaScript Plugins -->
     <script src="/admins/js/libs/jquery-1.8.3.min.js"></script>
     <script src="/admins/js/libs/jquery.mousewheel.min.js"></script>
     <script src="/admins/js/libs/jquery.placeholder.min.js"></script>
     <script src="/admins/custom-plugins/fileinput.js"></script>
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
     <!-- jQuery-UI Dependent Scripts -->
     <script src="/admins/jui/js/jquery-ui-1.9.2.min.js"></script>
     <script src="/admins/jui/jquery-ui.custom.min.js"></script>
@@ -572,14 +335,10 @@
 
     <!-- Demo Scripts (remove if not needed) -->
     <script src="/admins/js/demo/demo.dashboard.js"></script>
-<<<<<<< HEAD
-    @show
-=======
 @show
 
 @section('myJs')
 
 @show
->>>>>>> b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
 </body>
 </html>

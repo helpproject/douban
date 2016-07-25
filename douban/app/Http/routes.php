b34474ec0f4e889ba5ed6dd5b8aca9015e4b62a8
hejianyu <199640319@qq.com>
2016-07-25 15:49:50 +0800

    0.1

--- douban/app/Http/routes.php
+++ douban/app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller('/admin/user', 'UserController');
+Route::controller('/admin/attention', 'AttentionController');
+Route::controller('/admin/tag', 'TagController');
---
+++ douban/database/migrations/2016_07_25_054623_create_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tag')->connect('标签');
+            $table->integer('user_id')->connect('用户id');
+            $table->integer('book_id')->connect('图书id');
+            $table->tinyInteger('status')->comment('状态0下架1上架')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tags');
+    }
+}

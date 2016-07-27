145498ab9eefa845c8aba1d6710fe68b7f9b0d06
hejianyu <199640319@qq.com>
2016-07-27 15:31:44 +0800

    aa

--- douban/database/migrations/2016_07_23_183512_create_cates_table.php
+++ douban/database/migrations/2016_07_23_183512_create_cates_table.php
@@ -12,14 +12,14 @@ class CreateCatesTable extends Migration
      */
     public function up()
     {
-        // Schema::create('cates', function (Blueprint $table) {
-        //     $table->increments('id')->comment('主键自增id');
-        //     $table->string('name')->comment('名称');
-        //     $table->integer('pid')->comment('父级分类id');
-        //     $table->string('path')->comment('路径');
-        //     $table->tinyInteger('status')->comment('状态 1为成功 0为失败');
-        //     $table->timestamps();
-        // });
+//         Schema::create('cates', function (Blueprint $table) {
+//             $table->increments('id')->comment('主键自增id');
+//             $table->string('name')->comment('名称');
+//             $table->integer('pid')->comment('父级分类id');
+//             $table->string('path')->comment('路径');
+//             $table->tinyInteger('status')->comment('状态 1为成功 0为失败');
+//             $table->timestamps();
+//         });
     }
 
     /**
